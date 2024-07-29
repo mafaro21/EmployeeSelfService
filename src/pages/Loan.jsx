b86760c3730ff71ff4f7a8b81b11bcda0dff63b9
mafaro21 <mafaro2105@gmail.com>
2024-07-29 20:05:38 +0200

    R-H-F for Loan applications

--- src/pages/Loan.jsx
+++ src/pages/Loan.jsx
@@ -10,58 +10,32 @@ import {
     AlertDescription,
 
 } from '@chakra-ui/react'
+import { useForm } from "react-hook-form"
 
 export default function Loan() {
 
-    const [reason, setReason] = useState('')
     const [currency, setCurrency] = useState('ZWL')
-
-    const [accName, setAccName] = useState('')
-    const [bank, setBank] = useState('')
-    const [accNum, setAccNum] = useState('')
-    const [branchName, setBranchName] = useState('')
-    const [branchCode, setBranchCode] = useState('')
     const [declaration, setDeclaration] = useState(false)
 
-    const [amount, setAmount] = useState('')
-    const [installment, setInstallment] = useState('')
-    const [gross, setGross] = useState('')
-    const [net, setNet] = useState('')
-
-    const [errorDiv, setErrorDiv] = useState(false) // error message from frontend validation
-    const [errorText, setErrorText] = useState('')  // text based on the error
     const [loading, setLoading] = useState(false) // loading circle when user has submitted their id
 
     const toast = useToast()
 
+    const { register, formState: { errors }, handleSubmit } = useForm()
+
 
     const bankDetails = [
-        { name: 'Account Name', margin: '4', value: setAccName, base: '2' },
-        { name: 'Bank Name', margin: '10', value: setBank, base: '8' },
-        { name: 'Account Number', margin: '0', value: setAccNum, base: '-2' },
-        { name: 'Branch Name', margin: '6', value: setBranchName, base: '4' },
-        { name: 'Branch Code', margin: '7', value: setBranchCode, base: '5' },
+        { name: 'Account Name', margin: '4', value: 'accName', base: '2' },
+        { name: 'Bank Name', margin: '10', value: 'bank', base: '8' },
+        { name: 'Account Number', margin: '0', value: 'accNum', base: '-2' },
+        { name: 'Branch Name', margin: '6', value: 'branchName', base: '4' },
+        { name: 'Branch Code', margin: '7', value: 'branchCode', base: '5' },
     ]
 
-    const handleSubmit = () => {
-        setErrorText('')
+    const onSubmit = (data) => {
+        console.log(data)
 
-        if (Validation()) {
-            console.log({
-                reason,
-                amount,
-                installment,
-                gross,
-                net,
-                accName,
-                bank,
-                accNum,
-                branchName,
-                branchCode,
-                declaration,
-                currency,
-            })
-
+        if (data) {
             toast({
                 title: 'Success',
                 description: 'Looks good',
@@ -70,61 +44,14 @@ export default function Loan() {
                 isClosable: true,
                 position: 'top-right'
             })
-
-
         }
-        if (!Validation()) {
-            notVal()
-        }
-
     }
-
-    const Validation = () => {
-        if (!reason.trim()) {
-            setErrorDiv(true)
-            setErrorText('Please select reason')
-        }
-        else if (!amount.trim() || !installment.trim() || !gross.trim() || !net.trim()) {
-            setErrorDiv(true)
-            setErrorText('Please fill out loan and salary details')
-        }
-        else if (!accName.trim() || !branchName.trim() || !branchCode.trim() || !bank.trim() || !accNum.trim()) {
-            setErrorDiv(true)
-            setErrorText('Please input your banking details')
-        }
-        else if (declaration === false) {
-            setErrorDiv(true)
-            setErrorText('Please agree to the declaration')
-        }
-        else return true
-    }
-
-    const notVal = () => {
-        toast({
-            title: 'Failed to send',
-            // description: 'Make sure you have completed all fields',
-            description: errorText,
-            status: 'error',
-            duration: 4000,
-            isClosable: true,
-        })
-    }
-
-    const errorMessage = errorDiv ?
-        <div style={{ color: "red", fontSize: "18px" }}>{errorText}</div>
-        : '';
 
     return (
         <>
             <Navbar />
             <Container maxW='1100px' pt='4' >
-                {errorDiv ?
-                    <Alert status='error' >
-                        <AlertIcon />
-                        {/* <AlertTitle>Failed to send</AlertTitle> */}
-                        <AlertDescription>{errorText}</AlertDescription>
-                    </Alert> : ''}
-                <FormControl isRequired>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <Box >
                         {/* <Center> */}
                         <Text fontSize={{ md: "3xl", base: "2xl" }}>
@@ -137,7 +64,7 @@ export default function Loan() {
                             placeholder='Select Reason:'
                             focusBorderColor='#FF6201'
                             borderRadius='xl'
-                            onChange={(e) => setReason(e.target.value)}
+                            {...register('reason')}
                         >
                             <option value='Medical'>Medical</option>
                             <option value='Education'>Education</option>
@@ -150,7 +77,7 @@ export default function Loan() {
                         <Text as='u' fontSize={{ md: 'xl' }}>Loan Details</Text>
                         <Flex>
                             <Text mt='2'>Choose Currency:</Text>
-                            <Select width={{ md: '8%' }} ml='4' onChange={(e) => setCurrency(e.target.value)} focusBorderColor='#FF6201'>
+                            <Select width={{ md: '8%' }} ml='4' {...register('currency')} onChange={(e) => setCurrency(e.target.value)} focusBorderColor='#FF6201'>
                                 <option value='ZWL'>ZWL</option>
                                 <option value='USD'>USD</option>
                             </Select>
@@ -167,7 +94,7 @@ export default function Loan() {
                                         mt={{ md: '4' }}
                                         size='md'
                                         width={{ md: '80%' }}
-                                        onChange={(e) => setAmount(e.target.value)}
+                                        {...register('loanAmount')}
                                         type='number' placeholder='enter loan amount'
                                         focusBorderColor='#FF6201'
                                         required
@@ -186,9 +113,10 @@ export default function Loan() {
                                         mt={{ md: '4' }}
                                         size='md'
                                         width={{ md: '80%' }}
-                                        onChange={(e) => setInstallment(e.target.value)}
+                                        {...register('loanInstallment')}
                                         type='number' placeholder='enter loan installment'
                                         focusBorderColor='#FF6201'
+                                        required
                                     />
                                 </InputGroup>
                             </Stack>
@@ -216,9 +144,10 @@ export default function Loan() {
                                         mt={{ md: '4' }}
                                         size='md'
                                         width={{ md: '80%' }}
-                                        onChange={(e) => setGross(e.target.value)}
+                                        {...register('gross')}
                                         type='number' placeholder='enter loan amount'
                                         focusBorderColor='#FF6201'
+                                        required
                                     />
                                 </InputGroup>
                             </Stack>
@@ -234,7 +163,7 @@ export default function Loan() {
                                         mt={{ md: '4' }}
                                         size='md'
                                         width={{ md: '80%' }}
-                                        onChange={(e) => setNet(e.target.value)}
+                                        {...register('net')}
                                         type='number' placeholder='enter loan installment'
                                         focusBorderColor='#FF6201'
                                         isRequired
@@ -257,8 +186,9 @@ export default function Loan() {
                                         size='md'
                                         width={{ md: '450px' }}
                                         type='number'
-                                        onChange={(e) => bank.value(e.target.value)}
+                                        {...register(bank.value)}
                                         focusBorderColor='#FF6201'
+                                        required
                                     />
 
                                 </Stack>
@@ -270,7 +200,7 @@ export default function Loan() {
                         <Text as='u' fontSize={{ md: 'xl' }}>Declaration</Text>
 
                         <Text mt='1'>By applying for this loan, the Borrower agrees to the terms set forth by the Lender, including the repayment of the loan amount with an agreed-upon interest rate. The Borrower also consents to any applicable fees associated with the loan. Funds will be disbursed to the Borrower's specified account upon approval, and the Borrower commits to adhering to the repayment schedule detailed in the agreement. Failure to meet these obligations may result in additional charges or legal action.</Text>
-                        <Checkbox mt='4' colorScheme='orange' onChange={() => setDeclaration(!declaration)} required>I agree and accept to the terms</Checkbox>
+                        <Checkbox mt='4' colorScheme='orange' required>I agree and accept to the terms</Checkbox>
 
 
                     </Box>
@@ -278,12 +208,11 @@ export default function Loan() {
                         size='md'
                         mt='5'
                         mb='10'
-                        onClick={handleSubmit}
                         type='submit'
                     >
                         Submit Loan Application
                     </Button>
-                </FormControl>
+                </form>
             </Container>
         </>
     )
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { React, useState } from 'react'
 import Navbar from '../components/Navbar'
 import { Container, Box, Text, Stack, Input, Flex, Button } from '@chakra-ui/react'
+import { useForm } from "react-hook-form"
 
 export default function Profile() {
     const [firstName, setFirstName] = useState('')
@@ -9,6 +10,7 @@ export default function Profile() {
     const [phone, setPhone] = useState('')
     const [branchCode, setBranchCode] = useState('')
     const [disabled, setDisabled] = useState(true)
+    const { register, formState: { errors }, handleSubmit } = useForm()
 
     const profile = [
         { name: 'First Name', margin: '9', value: setFirstName, base: '9', place: 'from state', type: 'text' },
@@ -18,6 +20,10 @@ export default function Profile() {
         { name: 'Branch Code', margin: '6', value: setBranchCode, base: '6', place: 'from state', type: 'text' },
     ]
 
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
         <>
             <Navbar />
@@ -26,49 +32,52 @@ export default function Profile() {
                 <Text fontSize={{ md: "3xl", base: "xl" }}>
                     User's Profile
                 </Text>
+                <form onSubmit={handleSubmit(onSubmit)}>
 
-                {profile.map((profile, key) => (
-                    <Flex mt='2'>
+                    {profile.map((profile, key) => (
+                        <Flex mt='2'>
 
-                        <Text mt={{ md: '6', base: '3' }}>{profile.name}:</Text>
-                        <Stack spacing={4} ml='10'>
-                            <Input
-                                mt={{ md: '4', base: '1' }}
-                                ml={{ md: profile.margin, base: profile.base }}
+                            <Text mt={{ md: '6', base: '3' }}>{profile.name}:</Text>
+                            <Stack spacing={4} ml='10'>
+                                <Input
+                                    mt={{ md: '4', base: '1' }}
+                                    ml={{ md: profile.margin, base: profile.base }}
+                                    size='md'
+                                    width={{ md: '450px' }}
+                                    type={profile.type}
+                                    onChange={(e) => profile.value(e.target.value)}
+                                    // {...register(profile.value)}
+                                    focusBorderColor='#FF6201'
+                                    value={profile.place}
+                                    disabled={disabled == true ? 'disabled' : null}
+                                />
+
+                            </Stack>
+                        </Flex>
+                    ))}
+
+                    <Button colorScheme={disabled ? 'blue' : 'red'}
+                        size='md'
+                        mt='6'
+                        onClick={() => setDisabled(!disabled)}
+
+                    >
+                        {disabled ? 'Edit Profile' : 'Cancel Editing'}
+                    </Button>
+
+                    {disabled ? null :
+                        <Box>
+                            <Button colorScheme='orange'
                                 size='md'
-                                width={{ md: '450px' }}
-                                type={profile.type}
-                                onChange={(e) => profile.value(e.target.value)}
-                                focusBorderColor='#FF6201'
-                                value={profile.place}
-                                disabled={disabled == true ? 'disabled' : null}
-                            />
+                                mt='6'
+                            // onClick={() => setDisabled(!disabled)}
 
-                        </Stack>
-                    </Flex>
-                ))}
-
-                <Button colorScheme={disabled ? 'blue' : 'red'}
-                    size='md'
-                    mt='6'
-                    onClick={() => setDisabled(!disabled)}
-
-                >
-                    {disabled ? 'Edit Profile' : 'Cancel Editing'}
-                </Button>
-
-                {disabled ? null :
-                    <Box>
-                        <Button colorScheme='orange'
-                            size='md'
-                            mt='6'
-                        // onClick={() => setDisabled(!disabled)}
-
-                        >
-                            Save Changes
-                        </Button>
-                    </Box>
-                }
+                            >
+                                Save Changes
+                            </Button>
+                        </Box>
+                    }
+                </form>
             </Container>
         </>
     )
